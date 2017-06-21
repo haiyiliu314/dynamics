c933cb2590acf916b9452d63ff5c892a830b26cd
haiyiliu314 <liuhai@umich.edu>
2017-06-20 21:23:07 -0400

    Add files via upload

--- constants.f90
+++ constants.f90
@@ -11,7 +11,9 @@
   double precision, parameter                  ::sigmat = 0.15d0, tstart = -5.0d0, & 
                                                E_excit = 1.0d-3, shift = 4.0d0
   double precision                             ::coul_mat(Ny, Ny) = 0.0d0, Et(Nt+1) = 0.0d0, &
-                                               time(Nt+1) = 0.0d0, y(Ny)=0.0d0, &
+                                               time(Nt+1) = 0.0d0, argument(Nt+1) = 0.0d0, &
+                                               y(Ny)=0.0d0, dexp_argument(Nt+1) = 0.0d0, &
+                                               exp_argument(Nt+1) = 0.0d0, &
                                                y_fine(N_fine) = 0.0d0, dy_fine, f(Ny) = 0.0d0, &
                                                ft(Nt+1) = 0.0d0
   complex*16                                   ::p(Ny) = 0.0d0, pt(Nt+1) = 0.0d0
@@ -52,6 +54,9 @@
     do Ndo = 1, Nt+1
       time(Ndo) = (dble(Ndo)-1.0d0)*dt
       Et(Ndo) = Etime(dble(Ndo))
+      argument(Ndo) = -(dble(Ndo)*dt+tstart)*(dble(Ndo)*dt+tstart)/(sigmat*sigmat)
+      dexp_argument(Ndo) = dexp(-(dble(Ndo)*dt+tstart)*(dble(Ndo)*dt+tstart)/(sigmat*sigmat))
+      exp_argument(Ndo) = exp(-(dble(Ndo)*dt+tstart)*(dble(Ndo)*dt+tstart)/(sigmat*sigmat))
     end do
     do Ndo = 1,Ny
       y(Ndo) = dy*(dble(Ndo) - 0.50d0)
@@ -97,18 +102,11 @@
     complex*16                                             ::p_via(Ny), p_sum(Ny), test(Ny)
     double precision                                       ::f_via(Ny), f_sum(Ny)
     complex*16                                             ::funcp(Ny)
-    integer                                                ::num = 1 , i1
+
     f_sum = matmul(TRANSPOSE(coul_mat), f_via)
     p_sum = matmul(TRANSPOSE(coul_mat), p_via)
     funcp = -(0.0d0,1.0d0)*(y*y*p_via - ((2.0d0*f_sum)*p_via) + shift*p_via - (0.0d0,1.0d0) * &
     gamma * p_via/Ebind - (1.0d0-2.0d0*f_via)*(p_sum+Etime(nt_via)))/hbar*Ebind
-    if (nt_via == dble(num)*2500) then
-      write(format_V, '(A12, I6, A18)')   '(SE24.16e3, ', Ny, '(", ",SE24.16e3))'
-      do i1 = 1, Ny
-        write(700, format_V) funcp(i1)
-      end do
-      num = num+1
-    end if
   end function funcp
 
   function funcf(nt_via, p_via)
@@ -119,50 +117,65 @@
      funcf = 0d0
   end function funcf
 
-  subroutine RK_pf(f, g, f1, g1, n)
+ subroutine RK(Pfreq_in, E_in,  f_in, p_in, n, &
+               Pfreq_out, E_out, f_out, p_out)
     double precision                                    ::n                    !time step
-    double precision, dimension(Ny), intent(in)         ::f1
-    double precision, dimension(Ny), intent(out)        ::f
+    double precision, dimension(Ny), intent(in)         ::f_in
+    double precision, dimension(Ny), intent(out)        ::f_out
     double precision, dimension(Ny)                     ::kf1, kf2, kf3, kf4
-    complex*16, dimension(Ny), intent(in)               ::g1
-    complex*16, dimension(Ny), intent(out)              ::g
-    complex*16, dimension(Ny)                           ::kg1, kg2, kg3, kg4
-    kg1 = dt * funcp(n,f1, g1)
-    kf1 = dt * funcf(n,g1)
-    kg2 = dt * funcp(n+0.5d0, f1 + kf1/2.0d0, g1 + kg1/2.0d0)
-    kf2 = dt * funcf(n+0.5d0, g1 + kg1/2.0d0)
-    kg3 = dt * funcp(n+0.5d0, f1 + kf2/2.0d0, g1 + kg2/2.0d0)
-    kf3 = dt * funcf(n+0.5d0, g1 + kg2/2.0d0)
-    kg4 = dt * funcp(n+1.0d0, f1 + kf3, g1 + kg3)
-    kf4 = dt * funcf(n+1.0d0, g1 + kg3)
-    g = g1 + kg1/6.0d0 +kg2/3.0d0 + kg3/3.0d0 + kg4/6.0d0
-    f = f1 + kf1/6.0d0 +kf2/3.0d0 + kf3/3.0d0 + kf4/6.0d0
-  end subroutine RK_pf
+    complex*16, dimension(Ny), intent(in)               ::p_in
+    complex*16, dimension(Ny), intent(out)              ::p_out
+    complex*16, dimension(Ny)                           ::kp1, kp2, kp3, kp4
 
-  subroutine RK_E(E_out, E_in, n)
-    double precision                                  ::n                    !time step
-    complex*16, intent(in)                            ::E_in(N_freq)
-    complex*16, intent(out)                           ::E_out(N_freq)
-    complex*16                                        ::k1(N_freq), k2(N_freq), k3(N_freq)&
-                                                      , k4(N_freq)
-    k1 = dt * Etime(n) * Ebind * zexp((0.0d0, 1.0d0)*(dt*n)*freqgrid)
-    k2 = dt * Etime(n+0.5d0) * Ebind * zexp((0.0d0, 1.0d0)*freqgrid*(dt*(n+0.5d0)))
-    k3 = dt * Etime(n+0.5d0) * Ebind * zexp((0.0d0, 1.0d0)*freqgrid*(dt*(n+0.5d0)))
-    k4 = dt * Etime(n+1.0d0) * Ebind * zexp((0.0d0, 1.0d0)*freqgrid*(dt*(n+1.0d0)))
-    E_out = E_in + k1/6.0d0 +k2/3.0d0 + k3/3.0d0 + k4/6.0d0
-  end subroutine RK_E
+    complex*16, intent(in)                              ::E_in(N_freq)
+    complex*16, intent(out)                             ::E_out(N_freq)
+    complex*16                                          ::kE1(N_freq), kE2(N_freq), kE3(N_freq)&
+                                                        , kE4(N_freq)
 
-  subroutine RK_P(P_out, P_in, n)
-    double precision                                  ::n                    !time step
-    complex*16, intent(in)                            ::P_in(N_freq)
-    complex*16, intent(out)                           ::P_out(N_freq)
-    complex*16                                        ::k1(N_freq), k2(N_freq), k3(N_freq)&
-                                                      , k4(N_freq)
-    k1 = dt * pt(n) * Ebind* zexp((0.0d0, 1.0d0)*(dt*n)*freqgrid)
-    k2 = dt * pt(n) * Ebind* zexp((0.0d0, 1.0d0)*freqgrid*(dt*(n+0.5d0)))
-    k3 = dt * pt(n) * Ebind* zexp((0.0d0, 1.0d0)*freqgrid*(dt*(n+0.5d0)))
-    k4 = dt * pt(n+1.0d0) * Ebind* zexp((0.0d0, 1.0d0)*freqgrid*(dt*(n+1.0d0)))
-    P_out = P_in + k1/6.0d0 +k2/3.0d0 + k3/3.0d0 + k4/6.0d0
-  end subroutine RK_P
+    complex*16, intent(in)                              ::Pfreq_in(N_freq)
+    complex*16, intent(out)                             ::Pfreq_out(N_freq)
+    complex*16                                          ::kPfreq1(N_freq), kPfreq2(N_freq), kPfreq3(N_freq)&
+                                                        , kPfreq4(N_freq), kPfreq1_hlp(N_freq), &
+                                                        kPfreq2_hlp(N_freq), kPfreq3_hlp(N_freq), &
+                                                        kPfreq4_hlp(N_freq) 
+
+    integer                                             ::num = 1 , i1
+
+    kp1 = dt * funcp(n,f_in, p_in)
+    kf1 = dt * funcf(n,p_in)
+    kE1 = dt * Etime(n) * Ebind * zexp((0.0d0, 1.0d0)*(dt*n+tstart)*freqgrid)
+    kPfreq1_hlp = dy*sum(y*p_in)/(2.0d0*pi)
+    kPfreq1 = dt * kPfreq1_hlp * zexp((0.0d0, 1.0d0)*(tstart+dt*n)*freqgrid)
+
+    kp2 = dt * funcp(n+0.50d0, f_in + kf1/2.0d0, p_in + kp1/2.0d0)
+    kf2 = dt * funcf(n+0.5d0, p_in + kp1/2.0d0)
+    kE2 = dt * Etime(n+0.5d0) * Ebind * zexp((0.0d0, 1.0d0)*freqgrid*(tstart+dt*(n+0.5d0)))
+    kPfreq2_hlp = dy*sum(y*(p_in + kp1/2.0d0))/(2.0d0*pi)
+    kPfreq2 = dt * kPfreq2_hlp * zexp((0.0d0, 1.0d0)*freqgrid*(tstart+dt*(n+0.5d0)))
+
+    kp3 = dt * funcp(n+0.5d0, f_in + kf2/2.0d0, p_in + kp2/2.0d0)
+    kf3 = dt * funcf(n+0.5d0, p_in + kp2/2.0d0)
+    kE3 = dt * Etime(n+0.5d0) * Ebind * zexp((0.0d0, 1.0d0)*freqgrid*(tstart+dt*(n+0.5d0)))
+    kPfreq3_hlp = dy*sum(y*(p_in+kp2/2.0d0))/(2.0d0*pi)
+    kPfreq3 = dt * kPfreq3_hlp * zexp((0.0d0, 1.0d0)*freqgrid*(tstart+dt*(n+0.5d0)))
+
+    kp4 = dt * funcp(n+1.0d0, f_in + kf3, p_in + kp3)
+    kf4 = dt * funcf(n+1.0d0, p_in + kp3)
+    kE4 = dt * Etime(n+1.0d0) * Ebind * zexp((0.0d0, 1.0d0)*freqgrid*(tstart+dt*(n+1.0d0)))
+    kPfreq4_hlp = dy*sum(y*(p_in + kp3))/(2.0d0*pi)
+    kPfreq4 = dt * kPfreq4_hlp * zexp((0.0d0, 1.0d0)*freqgrid*(tstart+dt*(n+1.0d0)))
+
+    p_out = p_in + kp1/6.0d0 +kp2/3.0d0 + kp3/3.0d0 + kp4/6.0d0
+    f_out = f_in + kf1/6.0d0 +kf2/3.0d0 + kf3/3.0d0 + kf4/6.0d0
+    E_out = E_in + kE1/6.0d0 +kE2/3.0d0 + kE3/3.0d0 + kE4/6.0d0
+    Pfreq_out = Pfreq_in + kPfreq1/6.0d0 +kPfreq2/3.0d0 + kPfreq3/3.0d0 + kPfreq4/6.0d0
+    if (int(n) == (num*2500)) then
+      write(format_V, '(A12, I6, A18)')   '(SE24.16e3, ', Ny, '(", ",SE24.16e3))'
+      do i1 = 1, 1
+        write(700, format_V) int(n)
+      end do
+      num = num+1
+    end if
+end subroutine RK
 
 end module constants
--- dynamics_main.f90
+++ dynamics_main.f90
@@ -2,27 +2,24 @@ program main
   use constants
   implicit none
   integer                                                       ::Ndo
+  double precision                                              ::suscept(N_freq)
   call constant  
   call coul_matrix
-
+  call readdata
   write(list_file, '(A)') 'pk.dat'           !p(t)
   open(unit=700,file=list_file)
   do Ndo = 1, Nt
-    ft(Ndo) = dy*sum(y*f)
-    pt(Ndo) = dy*sum(y*p)
-    call RK_pf(f, p, f, p, dble(Ndo))  
+    ft(Ndo) = dy*sum(y*f)/(2.0d0*pi)
+    pt(Ndo) = dy*sum(y*p)/(2.0d0*pi)
+    call RK(p_freq, E_freq,  f, p, dble(Ndo), &
+               p_freq, E_freq, f, p)
   end do
+  ft(Ndo) = dy*sum(y*f)/(2.0d0*pi)
+  pt(Ndo) = dy*sum(y*p)/(2.0d0*pi)
   close(700)
-    ft(Ndo) = dy*sum(y*f)
-    pt(Ndo) = dy*sum(y*p)
-  call readdata
-  do Ndo = 1, Nt
-    call RK_P(p_freq, p_freq, dble(Ndo))
-  end do
-  
-  do Ndo = 1, Nt+1
-    call RK_E(E_freq, E_freq, dble(Ndo))
-  end do
+
+  suscept = aimag(p_freq / E_freq)
+
 
   write(list_file, '(A)') 'pt.dat'           !p(t)
   open(unit=700,file=list_file)
@@ -48,6 +45,7 @@ program main
   end do
   close(700)
 
+
   write(list_file, '(A)') 'ft.dat'           !f(t)
   open(unit=700,file=list_file)
   write(format_V, '(A12, I6, A18)')   '(SE24.16e3, ', Nt+1, '(", ",SE24.16e3))'
@@ -64,6 +62,37 @@ program main
   end do
   close(700)
 
+  write(list_file, '(A)') 'time.dat'           !E(t)
+  open(unit=700,file=list_file)
+  write(format_V, '(A12, I6, A18)')   '(SE24.16e3, ', Nt+1, '(", ",SE24.16e3))'
+  do i1 = 1, Nt+1
+    write(700, format_V) time(i1)+tstart
+  end do
+  close(700)
+
+  write(list_file, '(A)') 'argument.dat'           !E(t)
+  open(unit=700,file=list_file)
+  write(format_V, '(A12, I6, A18)')   '(SE24.16e3, ', Nt+1, '(", ",SE24.16e3))'
+  do i1 = 1, Nt+1
+    write(700, format_V) argument(i1)
+  end do
+  close(700)
+
+  write(list_file, '(A)') 'dexp_argument.dat'           !E(t)
+  open(unit=700,file=list_file)
+  write(format_V, '(A12, I6, A18)')   '(SE24.16e3, ', Nt+1, '(", ",SE24.16e3))'
+  do i1 = 1, Nt+1
+    write(700, format_V) dexp_argument(i1)
+  end do
+  close(700)
+
+  write(list_file, '(A)') 'exp_argument.dat'           !E(t)
+  open(unit=700,file=list_file)
+  write(format_V, '(A12, I6, A18)')   '(SE24.16e3, ', Nt+1, '(", ",SE24.16e3))'
+  do i1 = 1, Nt+1
+    write(700, format_V) exp_argument(i1)
+  end do
+  close(700)
 
   write(list_file, '(A)') 'coul_mat.dat'           !coul_mat
   open(unit=700,file=list_file)
